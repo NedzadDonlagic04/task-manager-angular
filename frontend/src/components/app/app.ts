3648ab99607145661254d486b00dfc14da19a6b2
AmerJamakovic <kajmakovic.amer@gmail.com>
2025-08-13 01:01:19 +0200

    Implemented the dark-theme light-theme toggle

--- frontend/src/components/app/app.ts
+++ frontend/src/components/app/app.ts
@@ -27,5 +27,17 @@ export class App {
 
   toggleDarkTheme() {
     console.log('Toggling dark theme');
+    
+    this.isDarkTheme = !this.isDarkTheme;
+    
+    if(this.isDarkTheme)
+      {
+        document.body.classList.add('dark-theme');  
+      }
+    else
+      {
+        document.body.classList.remove('dark-theme');
+      }
+
   }
 }

f95a07a0b2ef4ab27b9274dd89817bbe1fae8e43
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-14 17:46:25 +0200

    Added statistics page

---
+++ frontend/src/components/task-statistics-page/task-statistics-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TaskStatisticsPage } from './task-statistics-page';
+
+describe('TaskStatisticsPage', () => {
+  let component: TaskStatisticsPage;
+  let fixture: ComponentFixture<TaskStatisticsPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TaskStatisticsPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TaskStatisticsPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

71c5b59a03453ce996487f9bb0d9027c1593758a
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-14 17:46:11 +0200

    Added add task page

---
+++ frontend/src/components/add-task-page/add-task-page.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task-page',
+  imports: [],
+  templateUrl: './add-task-page.html',
+  styleUrl: './add-task-page.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AddTaskPage {
+
+}

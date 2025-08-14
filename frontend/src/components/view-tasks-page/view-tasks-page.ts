f1dd46d419101329e7086f42c259153918b4f4b5
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-14 17:46:31 +0200

    Added view tasks page

---
+++ frontend/src/components/view-tasks-page/view-tasks-page.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-tasks-page',
+  imports: [],
+  templateUrl: './view-tasks-page.html',
+  styleUrl: './view-tasks-page.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ViewTasksPage {
+
+}

aff7d79a96c87b46bec6e37986972a8be7b11fe6
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-16 15:26:25 +0200

    Added http client and native date providers

--- frontend/src/components/app/app.config.ts
+++ frontend/src/components/app/app.config.ts
@@ -1,16 +1,20 @@
 import {
-	ApplicationConfig,
-	provideBrowserGlobalErrorListeners,
-	provideZoneChangeDetection,
-} from '@angular/core';
-import { provideRouter } from '@angular/router';
+    ApplicationConfig,
+    provideBrowserGlobalErrorListeners,
+    provideZoneChangeDetection,
+} from "@angular/core";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
+import { routes } from "./app.routes";
+import { provideNativeDateAdapter } from "@angular/material/core";
+import { provideHttpClient } from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
-	providers: [
-		provideBrowserGlobalErrorListeners(),
-		provideZoneChangeDetection({ eventCoalescing: true }),
-		provideRouter(routes),
-	],
+    providers: [
+        provideBrowserGlobalErrorListeners(),
+        provideNativeDateAdapter(),
+        provideHttpClient(),
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+    ],
 };

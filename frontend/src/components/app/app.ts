040ad0572780984992f237007e3400f656f5d198
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-20 01:36:25 +0200

    Made home page start button navigate to add task

--- frontend/src/components/app/app.ts
+++ frontend/src/components/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from "@angular/core";
-
-//Amer Just putting in some angular material components to test
+import { Component, OnInit } from "@angular/core";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIcon, MatIconModule } from "@angular/material/icon";
 import { MatCardModule } from "@angular/material/card";
 import { routes } from "./app.routes";
 import { RouterLink, RouterOutlet } from "@angular/router";
+import { StorageService } from "../../services/storage.service";
 
 @Component({
     selector: "app-root",
@@ -23,15 +22,33 @@ import { RouterLink, RouterOutlet } from "@angular/router";
         RouterOutlet,
     ],
 })
-export class App {
+export class App implements OnInit {
+    readonly themeStorageKey = "isDarkTheme";
+
     routerLinks = routes;
     isDarkTheme = false;
 
-    toggleDarkTheme() {
-        console.log("Toggling dark theme");
+    constructor(private storageService: StorageService) {}
 
+    ngOnInit(): void {
+        const themeStorageValue = this.storageService.getItem<boolean>(
+            this.themeStorageKey,
+        );
+
+        if (themeStorageValue) {
+            this.isDarkTheme = themeStorageValue;
+            this.applyCSSForTheme();
+        }
+    }
+
+    toggleDarkTheme(): void {
         this.isDarkTheme = !this.isDarkTheme;
+        this.storageService.setItem(this.themeStorageKey, this.isDarkTheme);
 
+        this.applyCSSForTheme();
+    }
+
+    applyCSSForTheme(): void {
         if (this.isDarkTheme) {
             document.body.classList.add("dark-theme");
         } else {

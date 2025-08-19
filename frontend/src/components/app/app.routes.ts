10232c5016a069ce5e893d30786d1a4fe279bf79
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-19 12:28:36 +0200

    Added update/:id route

--- frontend/src/components/app/app.routes.ts
+++ frontend/src/components/app/app.routes.ts
@@ -3,11 +3,13 @@ import { ViewTasksPage } from "../view-tasks-page/view-tasks-page";
 import { AddTaskPage } from "../add-task-page/add-task-page";
 import { TaskStatisticsPage } from "../task-statistics-page/task-statistics-page";
 import { HomePage } from "../home-page/home-page";
+import { UpdateTaskPage } from "../update-task-page/update-task-page";
 
 export const routes: Routes = [
     { path: "", title: "Home", component: HomePage },
     { path: "add-task", title: "Add Task", component: AddTaskPage },
     { path: "view-tasks", title: "View Tasks", component: ViewTasksPage },
+    { path: "update/:id", title: "Update Task", component: UpdateTaskPage },
     {
         path: "task-statistics",
         title: "Taks Statistics",

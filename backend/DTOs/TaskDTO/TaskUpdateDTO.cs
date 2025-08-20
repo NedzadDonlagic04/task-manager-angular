d03e321d72cfa7178a1dff9119df4c9fd8c62625
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-21 00:59:03 +0200

    Added attributed for validation

--- backend/DTOs/TaskDTO/TaskUpdateDTO.cs
+++ backend/DTOs/TaskDTO/TaskUpdateDTO.cs
@@ -1,11 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using Enums;
+using Validation;
+
 namespace DTOs
 {
     public class TaskUpdateDTO
     {
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 50 characters.")]
         public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(1_000, ErrorMessage = "Description must be between 0 and 1_000 characters.")]
         public string Description { get; set; } = null!;
+
+        [MinimumFutureOffsetAttribute(1)]
         public DateTime? Deadline { get; set; }
+
+        [Required]
+        [EnumValidationAttribute(typeof(TaskStateEnum))]
         public int TaskStateId { get; set; }
+
+        [Required]
         public List<Guid> TagIds { get; set; } = new();
     }
 }

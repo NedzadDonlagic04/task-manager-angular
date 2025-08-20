ed7c8dd83950aadb71f7804aed30a6e40bdfae82
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-20 23:21:04 +0200

    Updated forms to have frontend validation

--- frontend/src/app/components/task-form-base/task-form-base.ts
+++ frontend/src/app/components/task-form-base/task-form-base.ts
@@ -9,27 +9,55 @@ import TagDTO from "../../dtos/tag.dto";
 import { TagService } from "../../services/tag.service";
 import { TaskService } from "../../services/task.service";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { deadlineDateAheadValidator } from "../../validators/deadline-date-validator";
+import { deadlineTimeAheadAnHourValidator } from "../../validators/deadline-time-validator";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
     selector: "task-form-base",
-    imports: [],
+    imports: [MatButtonModule],
     templateUrl: "./task-form-base.html",
     styleUrl: "./task-form-base.css",
 })
 export abstract class TaskFormBase implements OnInit {
+    private readonly HOUR_IN_MILISECONDS = 3_600_000;
+
+    readonly initialFormValues = {
+        title: "",
+        description: "",
+        hasDeadline: false,
+        deadlineDate: new Date().toISOString(),
+        deadlineTime: new Date(
+            new Date().getTime() + this.HOUR_IN_MILISECONDS,
+        ).toISOString(),
+    };
+
     tags: TagDTO[] = [];
-    hasDeadline: boolean = false;
 
     taskFormGroup = new UntypedFormGroup({
-        title: new FormControl("", {
-            validators: [Validators.required],
+        title: new FormControl(this.initialFormValues.title, {
+            validators: [
+                Validators.required,
+                Validators.minLength(3),
+                Validators.maxLength(50),
+            ],
             nonNullable: true,
         }),
-        description: new FormControl("", {
-            validators: [Validators.required],
+        description: new FormControl(this.initialFormValues.description, {
+            validators: [Validators.maxLength(1_000)],
             nonNullable: true,
         }),
-        deadline: new FormControl(new Date().toISOString()),
+        hasDeadline: new FormControl(this.initialFormValues.hasDeadline, {
+            nonNullable: true,
+        }),
+        deadlineDate: new FormControl(this.initialFormValues.deadlineDate, {
+            validators: [deadlineDateAheadValidator("hasDeadline")],
+            nonNullable: true,
+        }),
+        deadlineTime: new FormControl(this.initialFormValues.deadlineTime, {
+            validators: [deadlineTimeAheadAnHourValidator("hasDeadline")],
+            nonNullable: true,
+        }),
     });
 
     constructor(
@@ -39,6 +67,11 @@ export abstract class TaskFormBase implements OnInit {
     ) {}
 
     ngOnInit(): void {
+        this.startFetchingTags();
+        this.attachValueChangedToHasDeadline();
+    }
+
+    startFetchingTags(): void {
         this.tagService.getTags().subscribe({
             next: (tags) => {
                 this.tags = tags;
@@ -59,6 +92,13 @@ export abstract class TaskFormBase implements OnInit {
         });
     }
 
+    attachValueChangedToHasDeadline(): void {
+        this.hasDeadlineControl?.valueChanges.subscribe(() => {
+            this.deadlineDateControl?.updateValueAndValidity();
+            this.deadlineTimeControl?.updateValueAndValidity();
+        });
+    }
+
     getSelectedTags(): number[] {
         const tagCheckBoxValues = this.taskFormGroup.value.tags!;
         const tagIds: number[] = [];
@@ -74,4 +114,41 @@ export abstract class TaskFormBase implements OnInit {
 
     abstract onTagsLoaded(): void;
     abstract onSubmit(): void;
+
+    get titleControl() {
+        return this.taskFormGroup.get("title");
+    }
+
+    get descriptionControl() {
+        return this.taskFormGroup.get("description");
+    }
+
+    get hasDeadlineControl() {
+        return this.taskFormGroup.get("hasDeadline");
+    }
+
+    get deadlineTimeControl() {
+        return this.taskFormGroup.get("deadlineTime");
+    }
+
+    get deadlineDateControl() {
+        return this.taskFormGroup.get("deadlineDate");
+    }
+
+    getDeadlineFromControls(): Date | null {
+        const dateStr: Date | null = this.deadlineDateControl?.value;
+        const timeStr: Date | null = this.deadlineTimeControl?.value;
+
+        if (!dateStr || !timeStr) {
+            return null;
+        }
+
+        const date = new Date(dateStr);
+        const time = new Date(timeStr);
+
+        date.setHours(time.getHours());
+        date.setMinutes(time.getMinutes());
+
+        return date;
+    }
 }

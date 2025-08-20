597e5babbc5a5cd41c69704fbb803bf408b6b3c7
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-20 23:18:24 +0200

    Added utility function useful for debugging

---
+++ frontend/src/app/utils/find-invalid-controls.ts
@@ -0,0 +1,15 @@
+import { UntypedFormGroup } from "@angular/forms";
+
+// Useful fucker for debugging
+export const findInvalidControls = (formGroup: UntypedFormGroup) => {
+    const invalidControls = [];
+    const controls = formGroup.controls;
+
+    for (const name in controls) {
+        if (controls[name].invalid) {
+            invalidControls.push(name);
+        }
+    }
+
+    return invalidControls;
+};

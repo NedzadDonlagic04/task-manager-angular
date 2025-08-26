d42856cee21c6c3d1158f41252cdd0f42a124730
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-27 00:23:54 +0200

    Moved tasks table into separate component

--- frontend/src/app/components/view-tasks-page/view-tasks-page.ts
+++ frontend/src/app/components/view-tasks-page/view-tasks-page.ts
@@ -1,35 +1,10 @@
-import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
-import TaskReadDTO from "../../dtos/task-read.dto";
-import { TaskService } from "../../services/task.service";
-import { DatePipe } from "@angular/common";
-import { Router } from "@angular/router";
+import { Component } from "@angular/core";
+import { ViewTasksTable } from "../view-tasks-table/view-tasks-table";
 
 @Component({
     selector: "app-view-tasks-page",
-    imports: [DatePipe],
+    imports: [ViewTasksTable],
     templateUrl: "./view-tasks-page.html",
     styleUrl: "./view-tasks-page.css",
 })
-export class ViewTasksPage implements OnInit {
-    tasks: TaskReadDTO[] = [];
-
-    constructor(
-        private TaskService: TaskService,
-        private router: Router,
-    ) {}
-
-    ngOnInit(): void {
-        this.TaskService.getTasks().subscribe({
-            next: (tasks: TaskReadDTO[]) => {
-                this.tasks = tasks;
-            },
-            error: (error: any) => {
-                console.error("Error fetching tasks:", error);
-            },
-        });
-    }
-
-    navigatoToUpdatePage(taskId: string): void {
-        this.router.navigate(["/update", taskId]);
-    }
-}
+export class ViewTasksPage {}

071bf4f9dd31cc1d5b9e6a10149b6c1d9b0b94c8
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-27 13:56:13 +0200

    Added sorting and pagination

--- frontend/src/app/components/view-tasks-table/view-tasks-table.ts
+++ frontend/src/app/components/view-tasks-table/view-tasks-table.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import {
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    Component,
+    OnInit,
+    ViewChild,
+} from "@angular/core";
 import {
     MatHeaderCell,
     MatHeaderRow,
@@ -11,13 +17,13 @@ import {
 } from "@angular/material/table";
 import { TaskService } from "../../services/task.service";
 import TaskReadDTO from "../../dtos/task-read.dto";
-import { TasksTableDataSource } from "../../services/tasks-table-data-source.service";
 import { TaskTableRowData } from "../../services/task-table-row-data.service";
 import { DatePipe } from "@angular/common";
 import { MatIcon } from "@angular/material/icon";
 import { CdkTableModule } from "@angular/cdk/table";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatSortModule } from "@angular/material/sort";
+import { MatPaginator, MatPaginatorModule } from "@angular/material/paginator";
+import { MatSort, MatSortModule } from "@angular/material/sort";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "app-view-tasks-table",
@@ -30,25 +36,35 @@ import { MatSortModule } from "@angular/material/sort";
         MatTableModule,
         DatePipe,
         MatIcon,
+        MatPaginatorModule,
+        MatSortModule,
     ],
     templateUrl: "./view-tasks-table.html",
     styleUrl: "./view-tasks-table.css",
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ViewTasksTable implements OnInit {
-    displayedTasksColumns: string[] = [
+export class ViewTasksTable implements OnInit, AfterViewInit {
+    readonly NOT_SET_MESSAGE = "<Not set>";
+
+    readonly displayedTasksColumns: string[] = [
         "rowNum",
         "title",
         "description",
         "deadline",
         "createdAt",
-        "taskState",
+        "taskStateName",
         "tagNames",
         "actions",
     ];
-    tasksDataSource: MatTableDataSource<TaskTableRowData>;
+    readonly tasksDataSource: MatTableDataSource<TaskTableRowData>;
 
-    constructor(private taskService: TaskService) {
+    @ViewChild(MatSort) sort!: MatSort;
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+    constructor(
+        private taskService: TaskService,
+        private router: Router,
+    ) {
         this.tasksDataSource = new MatTableDataSource<TaskTableRowData>();
     }
 
@@ -65,5 +81,14 @@ export class ViewTasksTable implements OnInit {
             error: (error: any) =>
                 console.error(`Error while loading tags -> ${error}`),
         });
+    }
+
+    ngAfterViewInit(): void {
+        this.tasksDataSource.sort = this.sort;
+        this.tasksDataSource.paginator = this.paginator;
+    }
+
+    navigatoToUpdatePage(taskId: string): void {
+        this.router.navigate(["/update", taskId]);
     }
 }

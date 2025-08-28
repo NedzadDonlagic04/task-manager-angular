f74594ca7e15d68ff31da6a228aa7f6f7869b4c9
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-28 18:26:10 +0200

    Fixed bugs in validators

--- frontend/src/app/validators/deadline-time-validator.ts
+++ frontend/src/app/validators/deadline-time-validator.ts
@@ -2,24 +2,35 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export const deadlineTimeAheadAnHourValidator = (
     hasDeadlineControlName: string,
+    deadlineDateControlName: string,
 ): ValidatorFn => {
     return (control: AbstractControl): ValidationErrors | null => {
         if (!control.parent) {
             return null;
         }
 
-        const hasDeadline = control.parent.get(hasDeadlineControlName);
+        const hasDeadlineControl = control.parent.get(hasDeadlineControlName);
+        const deadlineDateControl = control.parent.get(deadlineDateControlName);
 
-        if (hasDeadline && !hasDeadline.value) {
+        if (hasDeadlineControl && !hasDeadlineControl.value) {
             return null;
         }
 
-        const deadline = new Date(control.value);
+        const controlDate = new Date(control.value);
+
+        const deadlineTime = new Date(deadlineDateControl?.value);
+        deadlineTime.setHours(
+            controlDate.getHours(),
+            controlDate.getMinutes(),
+            0,
+            0,
+        );
+
         const now = new Date();
         const HOUR_IN_MILISECONDS = 3_600_000;
         const oneHourFromNow = new Date(now.getTime() + HOUR_IN_MILISECONDS);
 
-        if (deadline <= oneHourFromNow) {
+        if (deadlineTime <= oneHourFromNow) {
             return { deadlineTimeAhead: true };
         }
 

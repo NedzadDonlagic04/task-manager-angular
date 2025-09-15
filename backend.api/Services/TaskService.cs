09351372b090b36bc303ab5c0a6d1879f0dadb89
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-09-15 02:01:55 +0200

    Refactored from ReloadAsync() to Reference().LoadAsync()

--- backend.api/Services/TaskService.cs
+++ backend.api/Services/TaskService.cs
@@ -125,7 +125,7 @@ namespace Services {
                 taskToUpdate.Tags.Add(tag);
             }
 
-            await _context.Entry(taskToUpdate.TaskState).ReloadAsync();
+            await _context.Entry(taskToUpdate).Reference(task => task.TaskState).LoadAsync();
             await _context.SaveChangesAsync();
 
             var updatedTask = new TaskReadDTO()

d1082245b62f09710beb40d825109bc34f046652
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-10-12 10:32:59 +0200

    Imported reflect-metadata globally

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/components/app/app.config';
 import { App } from './app/components/app/app';
+/*
+    This is either gonna be imported globally
+    or in every spot where class-transformer is used
+
+    I opted for the former
+*/
+import 'reflect-metadata';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));

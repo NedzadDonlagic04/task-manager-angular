ba217d5e327d200afba26de117df78ec3b3d0c67
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-10-24 23:41:58 +0200

    Added new migration

---
+++ backend.api/Migrations/20251024214102_AddedNewValueToTaskStateEnum.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNewValueToTaskStateEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TaskState",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 0, "Uninitialized" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TaskState",
+                keyColumn: "Id",
+                keyValue: 0);
+        }
+    }
+}

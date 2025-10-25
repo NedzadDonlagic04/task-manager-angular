5e235c316b7d51de26c1a19b5c9cdf77c6eeedc1
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-10-25 22:21:35 +0200

    Move Utils to Backend.Shared

---
+++ backend/Backend.Shared/Utils/Results.cs
@@ -0,0 +1,49 @@
+namespace Backend.Shared.Utils;
+
+public sealed class Result
+{
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public List<string> Errors { get; } = new List<string>();
+
+    private Result()
+    {
+        IsSuccess = true;
+    }
+
+    private Result(List<string> errors)
+    {
+        IsSuccess = false;
+        Errors = errors;
+    }
+
+    public static Result Success() => new Result();
+
+    public static Result Failure(string error) => new Result(new List<string> { error });
+}
+
+public sealed class Result<T>
+{
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public T? Value { get; }
+    public List<string> Errors { get; } = new List<string>();
+
+    private Result(T value)
+    {
+        IsSuccess = true;
+        Value = value;
+    }
+
+    private Result(List<string> errors)
+    {
+        IsSuccess = false;
+        Errors = errors;
+    }
+
+    public static Result<T> Success(T value) => new Result<T>(value);
+
+    public static Result<T> Failure(string error) => new Result<T>(new List<string>() { error });
+
+    public static Result<T> Failure(List<string> errors) => new Result<T>(errors);
+}

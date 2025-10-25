63ab4cf33606e60780be9f4c2a143678f06e1506
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-10-25 22:31:56 +0200

    Rename DbContext folder to Database

--- backend/Backend.Infastructure/Database/AppDbContext.cs
+++ backend/Backend.Infastructure/Database/AppDbContext.cs
@@ -1,8 +1,8 @@
-using Enums;
+using Backend.Domain.Enums;
+using Backend.Domain.Models;
 using Microsoft.EntityFrameworkCore;
-using Models;
 
-namespace DbContexts;
+namespace Backend.Infastructure.Database;
 
 public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
@@ -22,42 +22,26 @@ public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbCon
             })
             .ToArray();
 
-        List<Models.Tag> tagData =
+        List<Tag> tagData =
         [
-            new Models.Tag
-            {
-                Id = Guid.Parse("e280113f-a704-4e96-b909-30b322cc08b4"),
-                Name = "job",
-            },
-            new Models.Tag
-            {
-                Id = Guid.Parse("1ea6c135-be0f-4cd8-8029-863975601330"),
-                Name = "hobby",
-            },
-            new Models.Tag
-            {
-                Id = Guid.Parse("345b41fc-0019-46e0-b35d-e5a61ad76a4b"),
-                Name = "school",
-            },
-            new Models.Tag
-            {
-                Id = Guid.Parse("ea56ef56-0c7b-4995-9f7f-1333a363a9db"),
-                Name = "house",
-            },
+            new Tag { Id = Guid.Parse("e280113f-a704-4e96-b909-30b322cc08b4"), Name = "job" },
+            new Tag { Id = Guid.Parse("1ea6c135-be0f-4cd8-8029-863975601330"), Name = "hobby" },
+            new Tag { Id = Guid.Parse("345b41fc-0019-46e0-b35d-e5a61ad76a4b"), Name = "school" },
+            new Tag { Id = Guid.Parse("ea56ef56-0c7b-4995-9f7f-1333a363a9db"), Name = "house" },
         ];
 
         modelBuilder.Entity<TaskState>().HasData(taskStatesData);
         modelBuilder.Entity<Tag>().HasData(tagData);
 
         modelBuilder
-            .Entity<Models.Task>()
+            .Entity<Domain.Models.Task>()
             .HasMany(task => task.Tags)
             .WithMany(tag => tag.Tasks)
             .UsingEntity<TaskTag>();
     }
 
     public DbSet<Tag> Tag { get; private set; } = null!;
-    public DbSet<Models.Task> Task { get; private set; } = null!;
+    public DbSet<Domain.Models.Task> Task { get; private set; } = null!;
     public DbSet<TaskState> TaskState { get; private set; } = null!;
     public DbSet<TaskTag> TaskTag { get; private set; } = null!;
 }

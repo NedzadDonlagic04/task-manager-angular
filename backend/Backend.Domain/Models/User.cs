722bf0c9b943fa3825080d4e1cd41059b9d8a953
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-10-26 22:12:41 +0100

    Add new entities User and UserProfile

---
+++ backend/Backend.Domain/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend.Domain.Models;
+
+public sealed class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Username { get; set; } = null!;
+    public string HashedPassword { get; set; } = null!;
+    public UserProfile UserProfile { get; set; } = null!;
+    public List<Task> Tasks { get; set; } = [];
+}
---
+++ backend/Backend.Domain/Models/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Domain.Models;
+
+public sealed class UserProfile
+{
+    [Key]
+    public Guid UserId { get; set; }
+    public User User { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string? PictureUrl { get; set; }
+    public string? BannerUrl { get; set; }
+}

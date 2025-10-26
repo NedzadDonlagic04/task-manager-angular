8653314bcda6059da6f1fad03e58f9ed969b437c
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-10-26 12:00:15 +0100

    Add Backend.API layer

---
+++ backend/Backend.API/Program.cs
@@ -0,0 +1,36 @@
+using Backend.API.Extensions;
+using Backend.Application.Extensions;
+using Backend.Infastructure.Extensions;
+
+namespace Backend.API;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddOpenApi();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddAPI(builder.Configuration);
+        builder.Services.AddInfastructure(builder.Configuration);
+        builder.Services.AddApplication();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.MapOpenApi();
+        }
+
+        await app.Services.InitializeDatabaseAsync();
+
+        app.UseCors("AllowFrontend");
+        app.UseHttpsRedirection();
+        app.MapControllers();
+
+        app.Run();
+    }
+}

e8b7f67be99555bdbfa811fa2346aed68a6a7288
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-10-27 22:28:14 +0100

    Move entity configuration and seeding to Configurations/ folder classes

--- backend/Backend.Infastructure/Database/AppDbContext.cs
+++ backend/Backend.Infastructure/Database/AppDbContext.cs
@@ -1,6 +1,6 @@
+using System.Reflection;
 using Backend.Application.Interfaces;
-using Backend.Domain.Enums;
-using Backend.Domain.Models;
+using Backend.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Infastructure.Database;
@@ -9,42 +9,19 @@ public sealed class AppDbContext(DbContextOptions<AppDbContext> options)
     : DbContext(options),
         IAppDbContext
 {
+    public DbSet<Tag> Tag { get; private set; } = null!;
+    public DbSet<Domain.Entities.Task> Task { get; private set; } = null!;
+    public DbSet<TaskState> TaskState { get; private set; } = null!;
+    public DbSet<TaskTag> TaskTag { get; private set; } = null!;
+    public DbSet<User> User { get; private set; } = null!;
+    public DbSet<UserProfile> UserProfile { get; private set; } = null!;
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
         modelBuilder.Entity<TaskTag>().HasKey(taskTag => new { taskTag.TaskId, taskTag.TagId });
-
-        var taskStatesData = Enum.GetValues(typeof(TaskStateEnum))
-            .Cast<TaskStateEnum>()
-            .Where(taskStateEnum => taskStateEnum != TaskStateEnum.Uninitialized)
-            .Select(taskStateEnum => new TaskState
-            {
-                Id = (int)taskStateEnum,
-                Name = taskStateEnum.ToString(),
-            })
-            .ToArray();
-
-        List<Tag> tagData =
-        [
-            new Tag { Id = Guid.Parse("e280113f-a704-4e96-b909-30b322cc08b4"), Name = "job" },
-            new Tag { Id = Guid.Parse("1ea6c135-be0f-4cd8-8029-863975601330"), Name = "hobby" },
-            new Tag { Id = Guid.Parse("345b41fc-0019-46e0-b35d-e5a61ad76a4b"), Name = "school" },
-            new Tag { Id = Guid.Parse("ea56ef56-0c7b-4995-9f7f-1333a363a9db"), Name = "house" },
-        ];
-
-        modelBuilder.Entity<TaskState>().HasData(taskStatesData);
-        modelBuilder.Entity<Tag>().HasData(tagData);
-
-        modelBuilder
-            .Entity<Domain.Models.Task>()
-            .HasMany(task => task.Tags)
-            .WithMany(tag => tag.Tasks)
-            .UsingEntity<TaskTag>();
     }
-
-    public DbSet<Tag> Tag { get; private set; } = null!;
-    public DbSet<Domain.Models.Task> Task { get; private set; } = null!;
-    public DbSet<TaskState> TaskState { get; private set; } = null!;
-    public DbSet<TaskTag> TaskTag { get; private set; } = null!;
 }

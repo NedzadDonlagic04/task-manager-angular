9874b741472665d08b55f52024f2adc8927bdfa6
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-10-27 22:25:16 +0100

    Add user related entities

--- backend/Backend.Domain/Entities/User.cs
+++ backend/Backend.Domain/Entities/User.cs
@@ -1,10 +1,17 @@
-﻿namespace Backend.Domain.Entities;
+﻿using Backend.Shared.Interfaces;
 
-public sealed class User
+namespace Backend.Domain.Entities;
+
+public sealed class User : ITimeStampedEntity
 {
     public Guid Id { get; set; }
     public string Username { get; set; } = null!;
     public string HashedPassword { get; set; } = null!;
+
     public UserProfile UserProfile { get; set; } = null!;
+
     public List<Task> Tasks { get; set; } = [];
+
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
 }
--- backend/Backend.Domain/Entities/UserProfile.cs
+++ backend/Backend.Domain/Entities/UserProfile.cs
@@ -1,16 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Backend.Shared.Interfaces;
 
 namespace Backend.Domain.Entities;
 
-public sealed class UserProfile
+public sealed class UserProfile : ITimeStampedEntity
 {
-    [Key]
     public Guid UserId { get; set; }
     public User User { get; set; } = null!;
+
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
-    public string? Description { get; set; } = null!;
+    public string Description { get; set; } = null!;
     public string? PictureUrl { get; set; }
     public string? BannerUrl { get; set; }
+
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
 }

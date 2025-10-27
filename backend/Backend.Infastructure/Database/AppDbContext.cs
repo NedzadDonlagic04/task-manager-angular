fadeaaa64cfe6eb36beca5a4140c1c0c8c09aebc
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-10-27 22:48:52 +0100

    Override SaveChanges and SaveChangesAsync to add timestamping entries logic

--- backend/Backend.Infastructure/Database/AppDbContext.cs
+++ backend/Backend.Infastructure/Database/AppDbContext.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using Backend.Application.Interfaces;
 using Backend.Domain.Entities;
+using Backend.Shared.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Infastructure.Database;
@@ -16,6 +17,20 @@ public sealed class AppDbContext(DbContextOptions<AppDbContext> options)
     public DbSet<User> User { get; private set; } = null!;
     public DbSet<UserProfile> UserProfile { get; private set; } = null!;
 
+    public override int SaveChanges()
+    {
+        PrepareEntitiesForSave();
+
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        PrepareEntitiesForSave();
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
@@ -23,5 +38,22 @@ public sealed class AppDbContext(DbContextOptions<AppDbContext> options)
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
         modelBuilder.Entity<TaskTag>().HasKey(taskTag => new { taskTag.TaskId, taskTag.TagId });
+    }
+
+    private void PrepareEntitiesForSave()
+    {
+        foreach (var timeStampedEntry in ChangeTracker.Entries<ITimeStampedEntity>())
+        {
+            switch (timeStampedEntry.State)
+            {
+                case EntityState.Added:
+                    timeStampedEntry.Entity.CreatedAt = DateTimeOffset.UtcNow;
+                    timeStampedEntry.Entity.UpdatedAt = null;
+                    break;
+                case EntityState.Modified:
+                    timeStampedEntry.Entity.UpdatedAt = DateTimeOffset.UtcNow;
+                    break;
+            }
+        }
     }
 }

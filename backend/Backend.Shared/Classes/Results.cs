848339401444412916902ba561f6155878422a33
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-10-31 01:15:08 +0100

    Refactor to use more modern syntax

--- backend/Backend.Shared/Classes/Results.cs
+++ backend/Backend.Shared/Classes/Results.cs
@@ -4,7 +4,7 @@ public sealed class Result
 {
     public bool IsSuccess { get; }
     public bool IsFailure => !IsSuccess;
-    public List<string> Errors { get; } = new List<string>();
+    public List<string> Errors { get; } = [];
 
     private Result()
     {
@@ -17,9 +17,9 @@ public sealed class Result
         Errors = errors;
     }
 
-    public static Result Success() => new Result();
+    public static Result Success() => new();
 
-    public static Result Failure(string error) => new Result(new List<string> { error });
+    public static Result Failure(string error) => new([error]);
 }
 
 public sealed class Result<T>
@@ -27,7 +27,7 @@ public sealed class Result<T>
     public bool IsSuccess { get; }
     public bool IsFailure => !IsSuccess;
     public T? Value { get; }
-    public List<string> Errors { get; } = new List<string>();
+    public List<string> Errors { get; } = [];
 
     private Result(T value)
     {
@@ -41,9 +41,9 @@ public sealed class Result<T>
         Errors = errors;
     }
 
-    public static Result<T> Success(T value) => new Result<T>(value);
+    public static Result<T> Success(T value) => new(value);
 
-    public static Result<T> Failure(string error) => new Result<T>(new List<string>() { error });
+    public static Result<T> Failure(string error) => new([error]);
 
-    public static Result<T> Failure(List<string> errors) => new Result<T>(errors);
+    public static Result<T> Failure(List<string> errors) => new(errors);
 }

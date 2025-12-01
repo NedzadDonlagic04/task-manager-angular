abf5b64a3812fe5c2724b5724928391615ba4def
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-01 02:35:30 +0100

    Add implementation for IJwtService interface

---
+++ backend/Backend.API/Services/JwtService.cs
@@ -0,0 +1,34 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Backend.API.Options;
+using Backend.Application.Interfaces.Auth;
+using Backend.Domain.Entities.Users;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Backend.API.Services;
+
+public sealed class JwtService(IOptions<JwtOptions> options) : IJwtService
+{
+    private readonly JwtOptions _jwtOptions = options.Value;
+
+    public string IssueAccessToken(UserEntity user)
+    {
+        var claims = new List<Claim> { new(JwtRegisteredClaimNames.Sub, user.Id.ToString()) };
+
+        var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.Key));
+        var credentials = new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256);
+
+        var jwt = new JwtSecurityToken(
+            issuer: _jwtOptions.Issuer,
+            audience: _jwtOptions.Audience,
+            claims: claims,
+            signingCredentials: credentials
+        );
+
+        var accessToken = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+        return accessToken;
+    }
+}

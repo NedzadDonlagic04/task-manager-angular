4a32cfde4d061b9f1db452af6e487a172f46fdf0
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-01 02:30:47 +0100

    Add JwtOptions class and configuration in appsettings

---
+++ backend/Backend.API/Options/JwtOptions.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.API.Options;
+
+public sealed class JwtOptions
+{
+    public const string SectionName = "Jwt";
+
+    [Required]
+    public required string Key { get; init; }
+
+    [Required]
+    public required string Issuer { get; init; }
+
+    [Required]
+    public required string Audience { get; init; }
+
+    [Required]
+    public required string AccessTokenMinutes { get; init; }
+
+    [Required]
+    public required string RefreshTokenDays { get; init; }
+}

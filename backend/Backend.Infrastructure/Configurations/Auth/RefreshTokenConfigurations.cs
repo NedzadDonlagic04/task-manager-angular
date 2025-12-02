52e102d22c868161c311c644357b78a736113cbb
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-02 02:22:46 +0100

    Add refresh token entity configuration

---
+++ backend/Backend.Infrastructure/Configurations/Auth/RefreshTokenConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using Backend.Domain.Entities.Auth;
+using Backend.Infrastructure.Abstracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Backend.Infrastructure.Configurations.Auth;
+
+internal sealed class RefreshTokenConfigurations
+    : EntityTypeConfigurationAndSeeding<RefreshTokenEntity>
+{
+    protected override void ConfigureEntity(EntityTypeBuilder<RefreshTokenEntity> builder)
+    {
+        builder.HasKey(refreshToken => refreshToken.TokenHash);
+
+        builder.Property(refreshToken => refreshToken.TokenHash).IsRequired();
+
+        builder.Property(refreshToken => refreshToken.ExpiresAt).IsRequired();
+
+        builder.Property(task => task.CreatedAt).IsRequired().ValueGeneratedOnAdd();
+        builder.Property(task => task.UpdatedAt).IsRequired(false).ValueGeneratedOnAddOrUpdate();
+
+        builder.ToTable("RefreshTokens");
+    }
+}

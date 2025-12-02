82454770e1abbbc5e9b5a25532e2b0411c494d46
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-02 02:19:22 +0100

    Add user errors

--- backend/Backend.Application/Errors/Users/UserError.cs
+++ backend/Backend.Application/Errors/Users/UserError.cs
@@ -2,11 +2,4 @@
 
 namespace Backend.Application.Errors.Users;
 
-public sealed class UserError
-{
-    public static readonly Error LoginFailed = new(
-        "UserError.LoginFailed",
-        "Username or password incorrect",
-        ErrorType.Unauthorized
-    );
-}
+public sealed class UserError { }

8add5fe426bcb46bfd8159d089a87a42651b2a8d
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-02 02:21:04 +0100

    Move logic to AuthService

--- backend/Backend.Application/Services/Users/UserService.cs
+++ backend/Backend.Application/Services/Users/UserService.cs
@@ -1,37 +1,6 @@
-﻿using Backend.Application.DTOs.AuthDTO;
-using Backend.Application.Errors.Users;
-using Backend.Application.Interfaces;
-using Backend.Application.Interfaces.Auth;
+﻿using Backend.Application.Interfaces;
 using Backend.Application.Interfaces.Users;
-using Backend.Domain.Entities.Users;
-using Backend.Shared.Classes;
-using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Application.Services.Users;
 
-public sealed class UserService(IAppDbContext context, IJwtService jwtService) : IUserService
-{
-    public async Task<Result<LoginResultDTO>> Login(
-        LoginCredentialsDTO loginCredentials,
-        CancellationToken cancellationToken = default
-    )
-    {
-        var user = await context
-            .Set<UserEntity>()
-            .FirstOrDefaultAsync(
-                user => user.Username == loginCredentials.Username,
-                cancellationToken
-            );
-
-        if (user is null)
-        {
-            return UserError.LoginFailed;
-        }
-
-        var accessToken = jwtService.IssueAccessToken(user);
-
-        var loginResponseDTO = new LoginResultDTO { AccessToken = accessToken };
-
-        return Result<LoginResultDTO>.Success(loginResponseDTO);
-    }
-}
+public sealed class UserService(IAppDbContext context) : IUserService { }

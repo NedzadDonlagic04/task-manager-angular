8e242d9b6f9739a4b6bc5c3caf93bf30428c1f87
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-02 02:17:06 +0100

    Add DTOs used by auth controller

--- backend/Backend.Application/DTOs/AuthDTO/LoginResultDTO.cs
+++ backend/Backend.Application/DTOs/AuthDTO/LoginResultDTO.cs
@@ -3,4 +3,6 @@
 public sealed class LoginResultDTO
 {
     public required string AccessToken { get; init; }
+    public required string RefreshToken { get; init; }
+    public required DateTimeOffset AccessTokenExpiresAt { get; init; }
 }

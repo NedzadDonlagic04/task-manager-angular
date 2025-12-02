0d025e0de844379a67dd0108b49fdbf3ecfc2f14
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-02 02:19:16 +0100

    Add auth errors

---
+++ backend/Backend.Application/Errors/Auth/AuthError.cs
@@ -0,0 +1,12 @@
+﻿using Backend.Shared.Records;
+
+namespace Backend.Application.Errors.Auth;
+
+public sealed class AuthError
+{
+    public static readonly Error LoginFailed = new(
+        "AuthError.LoginFailed",
+        "Username or password incorrect",
+        ErrorType.Unauthorized
+    );
+}

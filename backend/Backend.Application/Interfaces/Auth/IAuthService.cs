1cce1fb0f18bbca6c2de65590089fa4c478a59da
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-02 02:20:20 +0100

    Add auth related services

---
+++ backend/Backend.Application/Interfaces/Auth/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Application.DTOs.AuthDTO;
+using Backend.Shared.Classes;
+
+namespace Backend.Application.Interfaces.Auth;
+
+public interface IAuthService
+{
+    Task<Result<LoginResultDTO>> Login(
+        LoginCredentialsDTO loginCredentials,
+        CancellationToken cancellationToken = default
+    );
+
+    Task<Result> Logout(
+        RevokeRefreshTokenDTO revokeRefreshTokenDTO,
+        CancellationToken cancellationToken = default
+    );
+}
--- backend/Backend.Application/Interfaces/Auth/IJwtService.cs
+++ backend/Backend.Application/Interfaces/Auth/IJwtService.cs
@@ -2,7 +2,19 @@
 
 namespace Backend.Application.Interfaces.Auth;
 
+public sealed class JwtTokens
+{
+    public required string AccessToken { get; init; }
+    public required DateTimeOffset AccessTokenExpiresAt { get; init; }
+
+    public required string RefreshToken { get; init; }
+    public required string RefreshTokenHash { get; init; }
+    public required DateTimeOffset RefreshTokenExpiresAt { get; init; }
+}
+
 public interface IJwtService
 {
-    string IssueAccessToken(UserEntity user);
+    JwtTokens IssueAccessToken(UserEntity user);
+
+    string HashRefreshToken(string refreshToken);
 }

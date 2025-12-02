529578bcb4d255f47e4238e3b9d576df50f71772
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-02 02:19:52 +0100

    Add refresh token entity

---
+++ backend/Backend.Domain/Entities/Auth/RefreshTokenEntity.cs
@@ -0,0 +1,16 @@
+﻿using Backend.Domain.Entities.Users;
+using Backend.Domain.Interfaces;
+
+namespace Backend.Domain.Entities.Auth;
+
+public sealed class RefreshTokenEntity : ITimeStampedEntity
+{
+    public string TokenHash { get; set; } = "";
+    public DateTimeOffset ExpiresAt { get; set; }
+
+    public Guid UserId { get; set; }
+    public UserEntity User { get; set; } = null!;
+
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+}

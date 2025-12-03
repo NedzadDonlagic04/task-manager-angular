1464759cf3ef4d76ae8968782280e523661b2f4d
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-03 14:19:36 +0100

    Add configuration for background service with options classes

---
+++ backend/Backend.Shared/Options/TaskDeadlineMonitorOptions.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Shared.Options;
+
+public sealed class TaskDeadlineMonitorOptions
+{
+    public const string SectionName = "BackgroundServices:TaskDeadlineMonitor";
+
+    [Required]
+    public required bool RunOnStart { get; init; }
+
+    [Required]
+    public required int CheckExpiredDeadlineSeconds { get; init; }
+}

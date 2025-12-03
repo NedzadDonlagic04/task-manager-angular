b1be2b589e1d592cd8da2cb0adac3b57d22a6c07
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-03 14:40:34 +0100

    Register task deadline monitor options and rename config parameter

--- backend/Backend.Application/Extensions/DependencyInjection.cs
+++ backend/Backend.Application/Extensions/DependencyInjection.cs
@@ -2,17 +2,28 @@
 using Backend.Application.Interfaces.Users;
 using Backend.Application.Services.Tasks;
 using Backend.Application.Services.Users;
+using Backend.Shared.Options;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Backend.Application.Extensions;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services,
+        IConfiguration config
+    )
     {
         services.AddScoped<ITagService, TagService>();
         services.AddScoped<ITaskService, TaskService>();
         services.AddScoped<ITaskStateService, TaskStateService>();
+
+        services
+            .AddOptions<TaskDeadlineMonitorOptions>()
+            .Bind(config.GetSection(TaskDeadlineMonitorOptions.SectionName))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
         services.AddHostedService<TaskDeadlineMonitorService>();
 
         services.AddScoped<IUserService, UserService>();

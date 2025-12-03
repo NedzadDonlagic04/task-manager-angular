a282590a1dfab5c358a9a5371fdf18a73e64a33d
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-03 14:39:10 +0100

    Add register account dto

---
+++ backend/Backend.Application/DTOs/AuthDTO/RegisterAccountDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend.Application.DTOs.AuthDTO;
+
+public sealed class RegisterAccountDTO
+{
+    public required string Username { get; init; }
+    public required string Password { get; init; }
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string Email { get; init; }
+}

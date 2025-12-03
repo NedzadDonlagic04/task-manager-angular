20a9a1e14be9c1518de907a092dde4812ce588a2
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-03 14:45:08 +0100

    Add class to represent a transaction

---
+++ backend/Backend.Infrastructure/Database/EfCoreTransaction.cs
@@ -0,0 +1,15 @@
+﻿using Backend.Application.Interfaces.Database;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Backend.Infrastructure.Database;
+
+public sealed class EfCoreTransaction(IDbContextTransaction transaction) : ITransaction
+{
+    public Task CommitAsync(CancellationToken cancellationToken = default) =>
+        transaction.CommitAsync(cancellationToken);
+
+    public Task RollbackAsync(CancellationToken cancellationToken = default) =>
+        transaction.RollbackAsync(cancellationToken);
+
+    public ValueTask DisposeAsync() => transaction.DisposeAsync();
+}

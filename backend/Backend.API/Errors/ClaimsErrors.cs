262206bbbac4a0e08df85cca82f1981b7cf523ac
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-04 13:39:32 +0100

    Add extension to more easily get UserId from JWT claims

---
+++ backend/Backend.API/Errors/ClaimsErrors.cs
@@ -0,0 +1,12 @@
+﻿using Backend.Shared.Records;
+
+namespace Backend.API.Errors;
+
+public static class ClaimsErrors
+{
+    public static readonly Error UserIdMissing = new(
+        "ClaimsError.UserIdMissing",
+        "The authenticated user's ID claim is missing",
+        ErrorType.Forbidden
+    );
+}

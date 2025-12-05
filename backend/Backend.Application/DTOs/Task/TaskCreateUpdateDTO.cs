eff4bfee5cb56efeb62dac9c9f76b21bbaf282de
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-05 01:05:08 +0100

    Update attribute error messages

--- backend/Backend.Application/DTOs/Task/TaskCreateUpdateDTO.cs
+++ backend/Backend.Application/DTOs/Task/TaskCreateUpdateDTO.cs
@@ -8,11 +8,11 @@ public sealed record TaskCreateUpdateDTO
     [StringLength(
         50,
         MinimumLength = 3,
-        ErrorMessage = "Title must be between 3 and 50 characters"
+        ErrorMessage = "{0} must be between {2} and {1} characters"
     )]
     public required string Title { get; init; }
 
-    [StringLength(1_000, ErrorMessage = "Description must be between 0 and 1_000 characters")]
+    [StringLength(1_000, ErrorMessage = "{0} cannot exceed {1} characters")]
     public required string Description { get; init; }
 
     [MinimumFutureOffset(1)]

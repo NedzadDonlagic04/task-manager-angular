a280c64c1e64fe0f5e3dec5d24dcdc3ef1af3cdd
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-09 23:59:43 +0100

    Add DTO for task bulk delete

---
+++ backend/Backend.Application/DTOs/Task/TaskBulkDeleteDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Backend.Application.DTOs.Task;
+
+public sealed record TaskBulkDeleteDTO
+{
+    public required Guid OwnerId { get; init; }
+    public required List<Guid> TaskIds { get; init; }
+}

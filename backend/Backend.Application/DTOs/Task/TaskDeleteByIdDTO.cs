e9794e1b4a91d927180b48e37a1fcaff3b1af2d4
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-10 00:00:55 +0100

    Add DTO for delete task by id

---
+++ backend/Backend.Application/DTOs/Task/TaskDeleteByIdDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Backend.Application.DTOs.Task;
+
+public sealed class TaskDeleteByIdDTO
+{
+    public required Guid OwnerId { get; init; }
+    public required Guid TaskId { get; init; }
+}

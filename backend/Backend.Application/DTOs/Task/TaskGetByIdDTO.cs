dc5d72b0dfc3d9d3576ebf3b640d34f147369175
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-10 00:01:04 +0100

    Add DTO for get task by id

---
+++ backend/Backend.Application/DTOs/Task/TaskGetByIdDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Backend.Application.DTOs.Task;
+
+public sealed record TaskGetByIdDTO
+{
+    public required Guid TaskId { get; init; }
+    public required Guid OwnerId { get; init; }
+}

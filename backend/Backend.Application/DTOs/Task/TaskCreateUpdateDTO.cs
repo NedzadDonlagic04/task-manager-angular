e7c81fd5a5f7367f12b9188276ebcb7742440f7e
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-12-10 00:01:27 +0100

    Update task create update DTO to include owner id

--- backend/Backend.Application/DTOs/Task/TaskCreateUpdateDTO.cs
+++ backend/Backend.Application/DTOs/Task/TaskCreateUpdateDTO.cs
@@ -1,10 +1,14 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using Backend.Application.Attributes;
 
 namespace Backend.Application.DTOs.Task;
 
 public sealed record TaskCreateUpdateDTO
 {
+    [JsonIgnore]
+    public required Guid OwnerId { get; set; }
+
     [StringLength(
         50,
         MinimumLength = 3,
